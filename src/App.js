9a5e0fb68ea3dc166f9e12e622e2ead83dad3b2b
tiarnola <tiarnola@gmail.com>
2021-11-16 23:15:29 +0000

    work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import RBSelect from './RBSelect';
 import ReactDOM from 'react-dom';
 import './Coin.css';
 var apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
-
+var = Secu;
 
 const x = new Boolean(false);
 
